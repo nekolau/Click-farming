c868b974377c1c52ceb34c62c30753e834ffdada
neko <43382195+nekolau@users.noreply.github.com>
2018-11-14 07:24:29 +0800

    Update neko02.py

--- neko02.py
+++ neko02.py
@@ -10,6 +10,6 @@ if __name__ == '__main__':
      i = 1;
      for i in range(1, 1000):
          print(i);
-         # 美国——2018年10月亚马逊美国站运动与户外玩具品类（Sports & Outdoors Toys）选品分析报告
+         # xxx
          url = "https://www.ikjzd.com/a/9993.html"
          resp = requests.get(url, headers=unicornHeader, timeout=30)
